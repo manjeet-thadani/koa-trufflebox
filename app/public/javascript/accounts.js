bd5b57890990f40a39c51f69aef42608a4d1730c
Manjeet Thadani <manjeet.thadani@gmail.com>
2019-09-08 12:48:18 +0530

    Added support to handle error while transfer of coins

--- app/public/javascript/accounts.js
+++ app/public/javascript/accounts.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-  var curraccount;
   var selectedAccount;
   var accounts;
 
@@ -7,45 +6,56 @@ $(document).ready(function () {
     accounts = response.accounts || [];
 
     for (let i = 0; i < accounts.length; i++) {
-      curraccount = accounts[i];
-      $('#options').append("<option value='" + curraccount + "'>" + curraccount + "</option>");
+      $('#options').append("<option value='" + accounts[i] + "'>" + accounts[i] + "</option>");
     }
-  })
+  });
 
   $('#submit').click(function () {
     selectedAccount = $('#options').val();
 
     $.get(`/accounts/${selectedAccount}`, function (response) {
-      const balance = response.balance ? response.balance : 0;
+      var balance = response.balance ? response.balance : 0;
 
       $('.select').removeClass("active");
       $('.send').addClass("active");
       $('#account').text(selectedAccount);
       $('#balance').text(balance);
 
-      var all_accounts = [...accounts] // clone of accounts
-      var current_account_index = all_accounts.indexOf(selectedAccount);
-      all_accounts.splice(current_account_index, 1); //remove the selected account from the list of accounts you can send to.
+      // clone of accounts
+      var allAccounts = [...accounts];
+      var currentAccountIndex = allAccounts.indexOf(selectedAccount);
+
+      // remove current account from list of all accounts before to which we need to send coin
+      allAccounts.splice(currentAccountIndex, 1);
 
       $('#all-accounts').addClass("active");
       var list = $('#all-accounts > ol');
 
-      for (let i = 0; i < all_accounts.length; i++) {
-        li = "<li>" + all_accounts[i] + "</li>";
+      for (let i = 0; i < allAccounts.length; i++) {
+        li = "<li>" + allAccounts[i] + "</li>";
         list.append(li)
       }
     })
-  })
+  });
 
   $('#send').click(function () {
     $('#status').text("Sending...");
 
     let amount = $('#amount').val();
     let receiver = $('#receiver').val();
-    
-    $.post('/transfer', { amount: amount, sender: selectedAccount, receiver: receiver }, function (response) {
-      $('#balance').text(response);
-      $('#status').text("Sent!!");
-    })
+
+    $.post('/transfer', { amount: amount, sender: selectedAccount, receiver: receiver })
+      .done(function (response) {
+        $('#status').text("Sent!!"); // set status to sent
+
+        // update balance of current account
+        $.get(`/accounts/${selectedAccount}`, function (response) {
+          var balance = response.balance ? response.balance : 0;
+          $('#balance').text(balance);
+        })
+      })
+      .fail(function (jqXHR, textStatus, errorThrown) {
+        $('#status').text("Error in sending coins");
+      });
   });
-})
+});
